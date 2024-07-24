19c2f8fe428278dbd21f752838d763ca8a5b9d73
Ron <swansonofmaine@aol.com>
2024-07-24 14:45:10 -0400

    add player group

--- shooterGame.py
+++ shooterGame.py
@@ -2,27 +2,35 @@ import pygame
 pygame.init()
 from player import Soldier
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = int(800 * 0.8)
-
-
 player = Soldier(200,200,3)
 player2 = Soldier(400,200,3)
+player_group = pygame.sprite.Group()
+player_group.add(player)
+player_group.add(player2)
 
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-pygame.display.set_caption("ShooterGame")
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(800 * 0.8)
+class ShooterGame(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+        pygame.display.set_caption("ShooterGame")
 
-run=True
-while run:
-    screen.blit(player.image,player.rect)
-    screen.blit(player2.image,player2.rect)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    pygame.display.update()
+    def run_game(self):
+        run=True        
+        while run:
+            for entity in player_group:
+                self.screen.blit(entity.image,entity.rect)
 
-pygame.quit()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+            pygame.display.update()
+            
+        pygame.quit()
 """make a pygame.group for payer and use it in the blit function."""
 
-
+if __name__ == '__main__':
+    sg = ShooterGame()
+    sg.run_game()
 
