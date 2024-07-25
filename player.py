e2b80208d5af911e2459c877e71c58ba61fc08b6
Ron <swansonofmaine@aol.com>
2024-07-25 03:41:48 -0400

    player movement added

--- player.py
+++ player.py
@@ -1,9 +1,21 @@
 import pygame
 
+#define player action variables
+moving_left = False
+moving_right = False
+
 class Soldier(pygame.sprite.Sprite):
-    def __init__(self,x,y,scale):
+    def __init__(self,sg,x,y,scale):
         pygame.sprite.Sprite.__init__(self)
+        self.sg = sg
         img = pygame.image.load("img/player/Idle/0.png")
         self.image = pygame.transform.scale(img,(int(img.get_width()) * scale,int(img.get_height())* scale))
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
+        self.speed = 1
+
+    def move(self):
+        if self.sg.moving_left == True:
+            self.rect.x -= self.speed
+        elif self.sg.moving_right == True:
+            self.rect.x += self.speed
--- shooterGame.py
+++ shooterGame.py
@@ -2,33 +2,58 @@ import pygame
 pygame.init()
 from player import Soldier
 
-player = Soldier(200,200,3)
-player2 = Soldier(400,200,3)
-player_group = pygame.sprite.Group()
-player_group.add(player)
-player_group.add(player2)
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(800 * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption("ShooterGame")
+
+#load images
+img = pygame.image.load('img/background/sky_cloud.png')
+bg_image = pygame.transform.scale(img,(SCREEN_WIDTH,SCREEN_HEIGHT))
+bg_rect = bg_image.get_rect()
+clock = pygame.time.Clock()
+FPS = 60
+
+
 class ShooterGame(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-        pygame.display.set_caption("ShooterGame")
+        self.player = Soldier(self,200,200,3)
+        self.player_group = pygame.sprite.Group()
+        self.player_group.add(self.player)
+        self.moving_left = False
+        self.moving_right = False
+        
 
     def run_game(self):
         run=True        
         while run:
-            for entity in player_group:
-                self.screen.blit(entity.image,entity.rect)
+            clock.tick(FPS)
+            screen.blit(bg_image,bg_rect)
+            self.player.move()
+            for entity in self.player_group:
+                screen.blit(entity.image,entity.rect)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.moving_right = True
+                    if event.key == pygame.K_LEFT:
+                        self.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.moving_right = False
+                    if event.key == pygame.K_LEFT:
+                        self.moving_left = False
+
             pygame.display.update()
+        
+         
             
         pygame.quit()
-"""make a pygame.group for payer and use it in the blit function."""
 
 if __name__ == '__main__':
     sg = ShooterGame()
